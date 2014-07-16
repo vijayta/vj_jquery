81e404d9ef55e0a340bb71ffbe7d0aaae98fa024
vijayta <vijayta.panchal@gmail.com>
2014-07-16 10:48:59 +0530

    input hint by OOPs

--- InputHint/js/inputHint.js
+++ InputHint/js/inputHint.js
@@ -1,17 +1,26 @@
 $(document).ready(function() { 
-  var $text = $('#search .input_text');
-  var $hint = $('#search label').remove().text();
-  $text.addClass('hint');
-  $text.focus(function() {
-    if($text.hasClass('hint')) {
-      $text.attr('value','');
-      $text.removeClass('hint');
-    }
-  });
-  $text.blur(function() {
-    var $textValue = $text.val().trim();
-    if($textValue == "") {
-      $text.attr('value', $hint).addClass('hint');
-    }
-  });
+  function $InputHint(textfield, labelVal){
+    var $text = textfield;
+    var $labelVal = labelVal;
+    $hint = $labelVal.remove().text();
+    $text.addClass('hint');
+    $text.bind('focus', function() {
+      if($text.hasClass('hint')) {
+        $text.attr('value','');
+        $text.removeClass('hint');
+      }
+    });
+    $text.bind('blur', function() {
+      var $textValue = $text.val().trim();
+      if($textValue == "") {
+        $text.attr('value', $hint).addClass('hint');
+      }
+    });  
+  }
+  // Assigning values
+  var textfield = $('#search .input_text');
+  var labelVal = $('#search label');
+
+  // Calling Function
+  new $InputHint(textfield, labelVal);
 });
