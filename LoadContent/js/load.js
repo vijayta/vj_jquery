869d241b70b7a2539e1bf8a7fa768e6dfb6d8284
vijayta <vijayta.panchal@gmail.com>
2014-07-17 17:09:42 +0530

    ajax content load

---
+++ LoadContent/js/load.js
@@ -0,0 +1,23 @@
+function LoadContent(blog) {
+  var $blog = blog, 
+      $heading = $blog.find('h3');
+  $heading.each(function() {
+    var $this = $(this),
+        $target = $('<div/>').insertAfter($this);
+    $this.data('target', $target);
+  });
+  $heading.click(function(event) {
+    event.preventDefault();
+    var $this = $(this),
+        $a = $this.find('a'),
+        $target = $this.data('target'),
+        href = $a.attr('href'),
+        id = '#' + href.split('#')[1];
+    $(this).data('target').load("data/blog.html " + id);
+  });
+}
+
+$(function() { 
+  var $blog = $('#blog');
+  new LoadContent($blog);
+});
