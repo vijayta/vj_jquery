69f8108660816d9279441c07bac569733641ed19
vijayta <vijayta.panchal@gmail.com>
2014-07-17 10:10:59 +0530

    slider issue fixed

--- ShowHide/js/blog.js
+++ ShowHide/js/blog.js
@@ -1,18 +1,18 @@
 function Slide(blogItem){
   var $blogItem = blogItem;
-  $($blogItem).each(function() {
-    var _this = $(this);
-    _this.find('h3').click(function(clickObject){
+  $blogItem.each(function() {
+    var $this = $(this);
+    $this.find('h3').click(function(clickObject) {
       clickObject.preventDefault();
-      _this.siblings().find('p').each(function() {
-          $(this).slideUp('400');
+      $this.siblings().find('p').each(function() {
+          // $(this).slideUp('400');
       });
-      _this.find('p').slideDown('400');
+      $this.find('p').slideDown('400');
     });
   });
 }
 
-$(document).ready(function() { 
-  var blogItem = $('#blog ul li');
-  new Slide(blogItem);
+$(function() { 
+  var $blogItem = $('#blog ul li');
+  new Slide($blogItem);
 });
