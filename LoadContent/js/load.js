210a10dcaa1900c1f6159c6c223c039421201b75
vijayta <vijayta.panchal@gmail.com>
2014-07-18 15:19:06 +0530

    ajax content exercise issue fixes

--- LoadContent/js/load.js
+++ LoadContent/js/load.js
@@ -1,23 +1,23 @@
-function LoadContent(blog) {
-  var $blog = blog, 
-      $heading = $blog.find('h3');
-  $heading.each(function() {
+function LoadDescription(blog) {
+  var $blog = blog;
+      this.heading = $blog.find('h3');
+      this.init();
+}
+LoadDescription.prototype.init = function() {
+  this.heading.each(function() {
     var $this = $(this),
         $target = $('<div/>').insertAfter($this);
     $this.data('target', $target);
   });
-  $heading.click(function(event) {
+  this.heading.click(function(event) {
     event.preventDefault();
     var $this = $(this),
-        $a = $this.find('a'),
-        $target = $this.data('target'),
-        href = $a.attr('href'),
-        id = '#' + href.split('#')[1];
-    $this.data('target').load("data/blog.html " + id);
+        postDetailLocation = 'data/' + $(this).children('a').attr('href').replace('#',' #');
+    $this.data('target').load(postDetailLocation);
   });
-}
+}      
 
 $(function() { 
   var $blog = $('#blog');
-  new LoadContent($blog);
+  new LoadDescription($blog);
 });
