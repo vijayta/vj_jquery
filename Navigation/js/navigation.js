870ca4e646617552ec9ca495e5554a1336dbf446
vijayta <vijayta.panchal@gmail.com>
2014-07-21 10:43:30 +0530

    navigation fixes

--- Navigation/js/navigation.js
+++ Navigation/js/navigation.js
@@ -1,15 +1,10 @@
 function Navigation(navItem) {
-  var $navItem = navItem;
-  $navItem.hover(function() {
-    var $this = $(this);
-    $this.find('ul').toggleClass('hover');
+  navItem.hover(function() {
+    $(this).find('ul').toggleClass('hover');
   });
 }
 
 $(document).ready(function() { 
-  // Assigning values
   var $navItem = $('#nav li');
-
-  // Calling Function
   new Navigation($navItem);
 });
