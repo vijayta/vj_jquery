d1bbaffc53b598ec0d6a08d38da193f17ee9aee1
vijayta <vijayta.panchal@gmail.com>
2014-07-31 11:26:22 +0530

    deligate excercise added

---
+++ AddItems/js/addItems.js
@@ -0,0 +1,25 @@
+function addItem(stackHolder, add) {
+  this.init(stackHolder, add);
+}
+addItem.prototype.init = function(stackHolder, add) {
+  var counter = 1;
+  add.bind('click', function() { 
+    stackHolder.prepend($('<div class="newItem"> This is Div "' + counter++ + '"</div>'));
+  });
+  stackHolder.delegate('div.newItem' , "click", function() {
+    var $this = $(this);
+    $this.toggleClass('highlight');
+    if($this.is(':first-child')) {
+      $this.remove();
+    }
+  });
+}
+
+$(document).ready(function() { 
+  // Assigning values
+  var $stackHolder = $('#stackHolder'), 
+      $add = $('#btnAdd');
+
+  // Calling Function
+  new addItem($stackHolder, $add);
+});
