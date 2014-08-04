239372ca7f89438bcf5e6f077d05103884e6c57d
vijayta <vijayta.panchal@gmail.com>
2014-08-04 10:51:46 +0530

    live funstion modified

--- AddItems/js/addItems.js
+++ AddItems/js/addItems.js
@@ -1,18 +1,22 @@
 function addItem(stackHolder, add) {
   this.init(stackHolder, add);
 }
+addItem.prototype.removeFirstChild = function(stackHolder, counter) {
+  stackHolder.delegate('div.newItem' , "click", function() {
+    var $this = $(this);
+    $this.toggleClass('highlight');
+    if($this.is(':first-child')) {
+      $this.remove().removeClass('highlight');
+      counter--;
+    }
+  });
+}
 addItem.prototype.init = function(stackHolder, add) {
   var counter = 1;
   add.bind('click', function() { 
     stackHolder.prepend($('<div class="newItem"> This is Div "' + counter++ + '"</div>'));
   });
-  stackHolder.delegate('div.newItem' , "click", function() {
-    var $this = $(this);
-    $this.toggleClass('highlight');
-    if($this.is(':first-child')) {
-      $this.remove();
-    }
-  });
+  this.removeFirstChild(stackHolder, counter);
 }
 
 $(document).ready(function() { 
