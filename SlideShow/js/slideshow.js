d6dea31a3babe3033f29552e65e0dacdb5c2f535
vijayta <vijayta.panchal@gmail.com>
2014-10-29 11:47:24 +0530

    slidershow modification

--- SlideShow/js/slideshow.js
+++ SlideShow/js/slideshow.js
@@ -1,38 +1,37 @@
-function SlideShow(slideshow) {
-  this.slideshow = slideshow;
+var SlideShow = function ($slideShow, $slideList) {
+  this.$slideShow = $slideShow;
+  this.$slideList = $slideList;
+  this.$length = this.$slideList.length;
+  this.$index = 0;
 }
-
-SlideShow.prototype.showItem = function() {
-  var _this = this;
-  if(this.loop == this.length) {
-    this.loop = 0;
-  }
-  this.items.eq(_this.loop).fadeIn(1000).delay(1000).fadeOut(1000,function() { 
-    _this.showItem();
-    _this.nav.text((_this.loop) + " of " + _this.length);
-  });
-  this.loop++;
-}
-
-SlideShow.prototype.updateCounter = function() {
-  this.nav = $('<div class="nav" />').insertAfter(this.slideshow);
-  this.nav.text((this.loop) + " of " + this.length);
-}
-
-SlideShow.prototype.init = function() {
-  $slideshow = this.slideshow.prependTo($('body'));
-  this.items = this.slideshow.find('li');
-  this.length = this.items.length;
-  this.items.hide();
-  this.loop = 0;
-  this.showItem();
+SlideShow.prototype.init = function () {
+  this.SliderPosition();
   this.updateCounter();
-  
+  this.showItem(this.$slideList.first());
+}
+SlideShow.prototype.SliderPosition = function () {
+  this.$slideShow.prependTo('body');
+  this.$slideList.hide();
+}
+SlideShow.prototype.updateCounter = function () {
+  $('<nav />').attr('id', 'sliderNavigation').insertAfter(this.$slideShow);
+}
+SlideShow.prototype.showItem = function ($nextSlide) {
+  var _this = this;
+  $('#sliderNavigation').text((_this.$index + 1) + " of " + _this.$length);
+    $nextSlide.fadeIn(1000).delay(1000).fadeOut(1000,function() { 
+    _this.$index++;
+    if(_this.$index < _this.$length) {
+      _this.showItem($(this).next());
+    }
+    else {
+      _this.$index = 0;
+      _this.showItem(_this.$slideList.first());
+    }
+  });
 }
 
-$(function() { 
-  var $slideshow = $('#slideshow');
-  var slides = new SlideShow($slideshow);
-  slides.init();
-
+$(function() {
+  var Slider = new SlideShow($('#slideshow'), $('#slideshow li'));
+  Slider.init();
 });
